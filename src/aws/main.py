a471818d5700868e384059d5942d78b41732ccfc
Mike O'Brien <mike.j.obrien@gmail.com>
2024-06-25 17:18:30 -0700

    initial files for ec2 instance

---
+++ src/aws/main.py
@@ -0,0 +1,27 @@
+\#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, NamedRemoteWorkspace, TerraformStack, TerraformOutput, RemoteBackend
+from cdktf_cdktf_provider_aws.provider import AwsProvider
+from cdktf_cdktf_provider_aws.instance import Instance
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        AwsProvider(self, "AWS", region="us-west-1")
+
+        instance = Instance(self, "compute",
+                            ami="ami-01456a894f71116f2",
+                            instance_type="t2.micro",
+                            )
+
+        TerraformOutput(self, "public_ip",
+                        value=instance.public_ip,
+                        )
+
+
+app = App()
+stack = MyStack(app, "aws_instance")
+
+app.synth()
